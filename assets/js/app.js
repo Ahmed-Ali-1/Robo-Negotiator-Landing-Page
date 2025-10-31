9fb08fdec0b84ce467312744948a79bb1a6445a0
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-11-01 02:35:14 +0500

    Hero Section Complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,28 @@
-let menuList = document.getElementById("menuList")
-menuList.style.maxHeight = "0px"
+// let menuList = document.getElementById("menuList")
+// menuList.style.maxHeight = "0px"
+
+// function toggleMenu() {
+//     if (menuList.style.maxHeight == "0px") {
+//         menuList.style.maxHeight = "300px"
+//     } else {
+//         menuList.style.maxHeight = "0px"
+//     }
+// }
+
+let menuList = document.getElementById("menuList");
+let menuIcon = document.getElementById("menuIcon");
+let icon = menuIcon.querySelector("i");
+
+menuList.style.maxHeight = "0px";
 
 function toggleMenu() {
-    if (menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "300px"
-    } else {
-        menuList.style.maxHeight = "0px"
-    }
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "300px";
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  } else {
+    menuList.style.maxHeight = "0px";
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
 }
